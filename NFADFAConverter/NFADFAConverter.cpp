68ba2b4d3b1587aa910c382c1586a2d1bfd6df23
Mohamed Khaled <es-mohamed.khaled.abbas20@alexu.edu.eg>
2023-12-01 01:15:48 +0200

    FIxing function "EClosure"

--- NFADFAConverter/NFADFAConverter.cpp
+++ NFADFAConverter/NFADFAConverter.cpp
@@ -3,25 +3,22 @@
 vector<Node *> NFADFAConverter::EClosure(vector<Node *> nodes)
 {
     stack<Node *> stack;
-    vector<Node *> eClosuere = vector<Node *>();
-    unordered_set<int> takenNodeNumbers = unordered_set<int>();
+    unordered_set<Node *> eClosuere;
     for (Node *node : nodes)
     {
         stack.push(node);
-        eClosuere.push_back(node);
-        takenNodeNumbers.insert(node->getNodeNumber());
+        eClosuere.insert(node);
     }
     while (!stack.empty())
     {
         Node *node = stack.top();
         stack.pop();
         for (Node *nextNode : node->getNextNodes((char)238))
-            if (takenNodeNumbers.find(nextNode->getNodeNumber()) != takenNodeNumbers.end())
+            if (eClosuere.find(nextNode) == eClosuere.end())
             {
                 stack.push(nextNode);
-                eClosuere.push_back(nextNode);
-                takenNodeNumbers.insert(nextNode->getNodeNumber());
+                eClosuere.insert(nextNode);
             }
     }
-    return eClosuere;
+    return vector<Node *>(eClosuere.begin(), eClosuere.end());
 }
