acf63333ec05885d75ff3557781ea59b7b5d7480
Mohamed Khaled <es-mohamed.khaled.abbas20@alexu.edu.eg>
2023-12-02 02:04:16 +0200

    Editing some functions related to DFA to have higher performance and less memory

--- NFADFAConverter/NFADFAConverter.cpp
+++ NFADFAConverter/NFADFAConverter.cpp
@@ -1,28 +1,52 @@
 #include "NFADFAConverter.h"
 
-typedef unordered_map<unordered_set<Node *>, unordered_map<char, unordered_set<Node *>>, HashUnorderedSetNodePointers> DFATransitions;
+template <typename T>
+unordered_set<T> unorderedSetsIntersection(const unordered_set<T> &first, const unordered_set<T> &second)
+{
+    unordered_set<T> result;
+    for (T element : first)
+        if (second.find(element) != second.end())
+            result.insert(element);
+    return result;
+}
 
-unordered_set<Node *> eClosure(unordered_set<Node *> nodes)
+string findHighestPeriorityRE(vector<string> &regularExpressions, unordered_map<string, int> &regularExpressionsPriorityMap)
+{
+    string highestPeriorityRE = regularExpressions[0];
+    int highestPeriority = regularExpressionsPriorityMap[highestPeriorityRE];
+    for (string regularExpression : regularExpressions)
+    {
+        int priority = regularExpressionsPriorityMap[regularExpression];
+        if (priority > highestPeriority)
+        {
+            highestPeriority = priority;
+            highestPeriorityRE = regularExpression;
+        }
+    }
+    return highestPeriorityRE;
+}
+
+unordered_set<Node *> findEClosure(unordered_set<Node *> nodes)
 {
     stack<Node *> stack;
-    unordered_set<Node *> eClosuere;
+    unordered_set<Node *> eClosure;
     for (Node *node : nodes)
     {
         stack.push(node);
-        eClosuere.insert(node);
+        eClosure.insert(node);
     }
     while (!stack.empty())
     {
         Node *node = stack.top();
         stack.pop();
         for (Node *nextNode : node->getNextNodes((char)238))
-            if (eClosuere.find(nextNode) == eClosuere.end())
+            if (eClosure.find(nextNode) == eClosure.end())
             {
                 stack.push(nextNode);
-                eClosuere.insert(nextNode);
+                eClosure.insert(nextNode);
             }
     }
-    return eClosuere;
+    return eClosure;
 }
 
 unordered_set<Node *> move(unordered_set<Node *> &sources, char input)
@@ -58,90 +82,88 @@ unordered_set<char> findAllPossibleInputs(Node *startNode)
     return allPossibleInputs;
 }
 
-DFATransitions findDFATransitions(Node *startNode, unordered_set<char> allPossibleInputs)
-{
-    unordered_set<unordered_set<Node *>, HashUnorderedSetNodePointers> allStates;
-    queue<unordered_set<Node *>> unmarkedStates;
-    DFATransitions statesTransitions;
-    unordered_set<Node *> nextState = eClosure({startNode});
-    allStates.insert(nextState);
-    unmarkedStates.push(nextState);
-    while (!unmarkedStates.empty())
-    {
-        unordered_set<Node *> currentState = unmarkedStates.front();
-        unmarkedStates.pop();
-        unordered_map<char, unordered_set<Node *>> transitions;
-        for (const char input : allPossibleInputs)
-        {
-            nextState = eClosure(move(currentState, input));
-            if (allStates.find(nextState) == allStates.end())
-            {
-                allStates.insert(nextState);
-                unmarkedStates.push(nextState);
-            }
-            transitions[input] = nextState;
-        }
-        statesTransitions.insert({currentState, transitions});
-    }
-    return statesTransitions;
-}
-
-template <typename T>
-unordered_set<T> unorderedSetsIntersection(unordered_set<T> &first, unordered_set<T> &second)
-{
-    unordered_set<T> result;
-    for (T element : first)
-        if (second.find(element) != second.end())
-            result.insert(element);
-    return result;
-}
-
-string findHighestPeriorityRE(vector<string> &regularExpressions, unordered_map<string, int> &regularExpressionsPriorityMap)
-{
-    string highestPeriorityRE = regularExpressions[0];
-    int highestPeriority = regularExpressionsPriorityMap[highestPeriorityRE];
-    for (string regularExpression : regularExpressions)
-    {
-        int priority = regularExpressionsPriorityMap[regularExpression];
-        if (priority > highestPeriority)
-        {
-            highestPeriority = priority;
-            highestPeriorityRE = regularExpression;
-        }
-    }
-    return highestPeriorityRE;
-}
-
-unordered_map<unordered_set<Node *>, string, HashUnorderedSetNodePointers> determineFinalStatesMap(
-    vector<unordered_set<Node *>> &dfaStates,
+unordered_map<int, string> determineFinalStatesMap(
+    unordered_map<unordered_set<Node *>, int, HashUnorderedSetNodePointers> dfaStatesNodesMap,
     unordered_map<Node *, string> finalNodesMap,
     unordered_map<string, int> &regularExpressionsPriorityMap)
 {
-    unordered_map<unordered_set<Node *>, string, HashUnorderedSetNodePointers> finalStatesMap;
+    unordered_map<int, string> finalStatesMap;
     unordered_set<Node *> finalNodes;
     for (auto const &finalNodesMapElement : finalNodesMap)
         finalNodes.insert(finalNodesMapElement.first);
-    for (unordered_set<Node *> dfaState : dfaStates)
+    for (auto const &dfaStatesNodesMapElement : dfaStatesNodesMap)
     {
-        unordered_set<Node *> finalNodesIntersection = unorderedSetsIntersection<Node *>(dfaState, finalNodes);
+        unordered_set<Node *> finalNodesIntersection = unorderedSetsIntersection<Node *>(dfaStatesNodesMapElement.first, finalNodes);
         if (finalNodesIntersection.empty())
             continue;
         vector<string> regularExpressions;
         for (Node *finalNode : finalNodesIntersection)
             regularExpressions.push_back(finalNodesMap[finalNode]);
-        finalStatesMap[dfaState] = findHighestPeriorityRE(regularExpressions, regularExpressionsPriorityMap);
+        finalStatesMap[dfaStatesNodesMapElement.second] = findHighestPeriorityRE(regularExpressions, regularExpressionsPriorityMap);
     }
     return finalStatesMap;
 }
 
+pair<unordered_map<int, unordered_map<char, int>>, unordered_map<int, string>> findDFAInfo(CombinedAutomaton nfa,
+                                                                                           unordered_map<string, int> &regularExpressionsPriorityMap,
+                                                                                           unordered_set<char> allPossibleInputs)
+{
+    int alias = 1;
+    unordered_map<unordered_set<Node *>, int, HashUnorderedSetNodePointers> statesNodesMap;
+    unordered_map<int, unordered_set<Node *>> statesAliasMap;
+    unordered_set<int> allStates;
+    queue<int> unmarkedStates;
+    unordered_map<int, unordered_map<char, int>> statesTransitions;
+    unordered_set<Node *> nextStateNodes = findEClosure({nfa.getStartNode()});
+    statesNodesMap[nextStateNodes] = alias;
+    statesAliasMap[alias] = nextStateNodes;
+    allStates.insert(alias);
+    unmarkedStates.push(alias);
+    alias++;
+    while (!unmarkedStates.empty())
+    {
+        int currentState = unmarkedStates.front();
+        unmarkedStates.pop();
+        unordered_map<char, int> transitions;
+        for (const char input : allPossibleInputs)
+        {
+            nextStateNodes = findEClosure(move(statesAliasMap[currentState], input));
+            if (allStates.find(statesNodesMap[nextStateNodes]) == allStates.end())
+            {
+                statesNodesMap[nextStateNodes] = alias;
+                statesAliasMap[alias] = nextStateNodes;
+                allStates.insert(alias);
+                unmarkedStates.push(alias);
+                alias++;
+            }
+            transitions[input] = statesNodesMap[nextStateNodes];
+        }
+        statesTransitions.insert({currentState, transitions});
+    }
+    return {statesTransitions, determineFinalStatesMap(statesNodesMap, nfa.getFinalNodesMap(), regularExpressionsPriorityMap)};
+}
+
+vector<unordered_set<int>> minimizeDFAStates(vector<unordered_set<int>> &statesGroups,
+                                             unordered_map<int, unordered_map<char, int>> &dfaTransitionsMap,
+                                             unordered_set<char> allPossibleInputs)
+{
+    // TODO
+    return vector<unordered_set<int>>();
+}
+
 CombinedAutomaton NFADFAConverter::convertNFAToDFA(CombinedAutomaton nfa, unordered_map<string, int> &regularExpressionsPriorityMap)
 {
-    Node *nfaStartNode = nfa.getStartNode();
-    unordered_set<char> allPossibleInputs = findAllPossibleInputs(nfaStartNode);
-    DFATransitions dfaTransitions = findDFATransitions(nfaStartNode, allPossibleInputs);
-    vector<unordered_set<Node *>> dfaStates;
-    for (auto const &dfaTransition : dfaTransitions)
-        dfaStates.push_back(dfaTransition.first);
-    unordered_map<unordered_set<Node *>, string, HashUnorderedSetNodePointers> finalStatesMap = determineFinalStatesMap(
-        dfaStates, nfa.getFinalNodesMap(), regularExpressionsPriorityMap);
+    unordered_set<char> allPossibleInputs = findAllPossibleInputs(nfa.getStartNode());
+    pair<unordered_map<int, unordered_map<char, int>>, unordered_map<int, string>> dfaInfo = findDFAInfo(nfa, regularExpressionsPriorityMap, allPossibleInputs);
+    unordered_map<int, unordered_map<char, int>> dfaTransitionsMap = dfaInfo.first;
+    unordered_map<int, string> dfaFinalStatesMap = dfaInfo.second;
+    unordered_set<int> dfaFinalStates;
+    unordered_set<int> dfaNonFinalStates;
+    for (auto const &dfaFinalStatesMapElement : dfaFinalStatesMap)
+        dfaFinalStates.insert(dfaFinalStatesMapElement.first);
+    for (auto const &dfaTransitionsMapElement : dfaTransitionsMap)
+        if (dfaFinalStates.find(dfaTransitionsMapElement.first) == dfaFinalStates.end())
+            dfaNonFinalStates.insert(dfaTransitionsMapElement.first);
+    vector<unordered_set<int>> statesGroups = vector<unordered_set<int>>{dfaFinalStates, dfaNonFinalStates};
+    statesGroups = minimizeDFAStates(statesGroups, dfaTransitionsMap, allPossibleInputs);
 }
--- NFADFAConverter/NFADFAConverter.h
+++ NFADFAConverter/NFADFAConverter.h
@@ -3,6 +3,8 @@
 
 #include "../AutomatonDataStructure/CombinedAutomaton.h"
 
+// TODO: check if it is better to make this a namespace and extract common reusable functions.
+
 class NFADFAConverter
 {
 public:
